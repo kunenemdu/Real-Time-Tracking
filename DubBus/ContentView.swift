ba9dce5c70857f2a0fb85ee201cdf5f818b584de
Mdu Kunene <kunene.mdu7@gmail.com>
2026-02-17 22:53:10 +0000

    Initial Commit

---
+++ DubBus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DubBus
+//
+//  Created by Thobekile Sibanda on 17/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DubBus/DubBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubBusApp.swift
+//  DubBus
+//
+//  Created by Thobekile Sibanda on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct DubBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
